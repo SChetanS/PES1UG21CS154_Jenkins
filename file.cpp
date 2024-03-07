897d68a7981beb5e6e0101c90bf9c684c4814d4c
SChetanS <149644828+SChetanS@users.noreply.github.com>
2024-03-07 13:14:30 +0530

    Add files via upload

---
+++ file.cpp
@@ -0,0 +1,29 @@
+pipeline {
+  agent any
+  stages {
+    stage('Build') {
+          steps {
+            build 'PES1UG21CS160-1'
+            sh 'g++ main1.cpp -o output'
+            }
+        }
+
+    stage('Test') {
+      steps {
+        sh './output'
+      }
+    }
+
+    stage('Deploy') {
+      steps {
+        echo 'deploy'
+      }
+    }  
+  }
+          
+  post{
+    failure{
+      error 'Pipeline failed'
+    }
+  }
+}
